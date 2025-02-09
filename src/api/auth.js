b2bc7aa42c4b70f39e5779e6bcce1f1b39e54640
théo <117741458+ewkt@users.noreply.github.com>
2025-02-09 18:00:13 +0100

    Rework requests and handle url params

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,19 +1,13 @@
-import * as cheerio from 'cheerio';
 import { authorizedRequest } from './request.js';
 
 //fetch cookies for the domain with advancded option to have cloudfare cookie
-export const fetchCookie = async (adv = false) => {
+export const fetchCookie = async () => {
     //fetch the standard cookies
     console.log('fetching cookies');
-    const response = await authorizedRequest(
-        "GET", 
-        `https://www.vinted.fr/how_it_works`, 
-        null, 
-        null, 
-        null,
-        false, 
-        false
-    );
+    const response = await authorizedRequest({
+        method: "GET", 
+        url: process.env.BASE_URL+"how_it_works", 
+    });
 
     const sessionCookies = response.headers.get('set-cookie');
     if (!sessionCookies) throw new Error("set-cookie headers not found in the response");
@@ -26,7 +20,7 @@ export const fetchCookie = async (adv = false) => {
         return acc;
     }, {});
 
-    const requiredCookies = ['access_token_web', 'v_sid', 'v_udt', '_vinted_fr_session'];
+    const requiredCookies = ['access_token_web', '_vinted_fr_session'];
     let cookieHeader = requiredCookies
         .filter(cookie => parsedCookies[cookie])
         .map(cookie => `${cookie}=${parsedCookies[cookie]}`)
--- src/api/request.js
+++ src/api/request.js
@@ -2,17 +2,13 @@ import fetch from 'node-fetch';
 import UserAgent from 'user-agents';
 
 //general fucntion to make an authorized request
-export const authorizedRequest = async (method, url, data, tokens, cookies, c = false, t = false, logging = true) => {
+export const authorizedRequest = async ({method, url, oldUrl = null , data = null, cookies = null, logs = true} = {}) => {
     try {
-        let urlObj = new URL(url);
         const headers = {
             "user-agent": new UserAgent().toString(),
-            "Host": urlObj.host,
             "Accept": "application/json, text/plain, */*",
             "Accept-Language": "en",
             "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Referer": `https://${urlObj.host}/`,
-            "Origin": `https://${urlObj.host}`,
             "DNT": "1",
             "Connection": "keep-alive",
             "Sec-Fetch-Dest": "empty",
@@ -21,46 +17,32 @@ export const authorizedRequest = async (method, url, data, tokens, cookies, c = 
             "Content-Length": "0",
             "Content-Type": "*/*",
         };
-
-        if (c) {
+        if (cookies) {
             headers["Cookie"] = cookies;
+            headers["Referer"] = oldUrl;
+            headers["Origin"] = oldUrl;
         }
-
-        if (t) {
-            headers["Authorization"] = `Bearer ${tokens.access}`;
-            headers["X-CSRF-Token"] = tokens.xcsrf;
-        }
-
         const options = {
-            "credentials": "include",
             "headers": headers,
             "method": method,
-            "mode": "cors",
-            "redirect": "manual",
-            "timeout": 5000
         };
-
         if (method !== 'GET' && method !== 'HEAD') {
             options.body = JSON.stringify(data);
         }
-
-        let response = await fetch(url, options);
-
-        if (logging){
+        if (logs) {
             console.log("making an authed request to " + url);
         }
+        let response = await fetch(url, options);
 
         while ([301, 302, 303, 307, 308].includes(response.status)) {
             const newUrl = response.headers.get('Location');
             console.log(`redirected to ${newUrl}`);
             response = await fetch(newUrl, options);
         }
-        
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-
-        if (response.headers.get('Content-Type').includes('text/html')) {
+        if (response.headers.get('Content-Type')?.includes('text/html')) {
             return response;
         }
 
